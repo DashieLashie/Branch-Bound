b38b3088fe1c35fa277539776e8c95ab1e63b2e3
Alex Gonzalez <durandalex1259@gmail.com>
2021-03-09 23:02:21 -0600

    Refactored

--- Branch&Bound.py
+++ Branch&Bound.py
@@ -1,172 +1,112 @@
-# Python3 program to solve
-# Traveling Salesman Problem using
-# Branch and Bound.
 import math
+limite_superior = float('inf')
 
-maxsize = float('inf')
+def crearFinal(camino_actual):
+    camino_final[:numero_nodos + 1] = camino_actual[:]
+    camino_final[numero_nodos] = camino_actual[0]
 
-
-# Function to copy temporary solution
-# to the final solution
-def copyToFinal(curr_path):
-    final_path[:N + 1] = curr_path[:]
-    final_path[N] = curr_path[0]
-
-
-# Function to find the minimum edge cost
-# having an end at the vertex i
-def firstMin(adj, i):
-    min = maxsize
-    for k in range(N):
+def primerMinimo(adj, i):
+    min = limite_superior
+    for k in range(numero_nodos):
         if adj[i][k] < min and i != k:
             min = adj[i][k]
-
     return min
 
-
-# function to find the second minimum edge
-# cost having an end at the vertex i
-def secondMin(adj, i):
-    first, second = maxsize, maxsize
-    for j in range(N):
+def segundoMinimo(adj, i):
+    primero, segundo = limite_superior, limite_superior
+    for j in range(numero_nodos):
         if i == j:
             continue
-        if adj[i][j] <= first:
-            second = first
-            first = adj[i][j]
+        if adj[i][j] <= primero:
+            segundo = primero
+            primero = adj[i][j]
 
-        elif (adj[i][j] <= second and
-              adj[i][j] != first):
-            second = adj[i][j]
+        elif (adj[i][j] <= segundo and
+              adj[i][j] != primero):
+            segundo = adj[i][j]
+    return segundo
 
-    return second
+def rec(adj, bound_actual, peso_actual, profundidad, camino_actual, visitado):
+    global costo
 
-
-# function that takes as arguments:
-# curr_bound -> lower bound of the root node
-# curr_weight-> stores the weight of the path so far
-# level-> current level while moving
-# in the search space tree
-# curr_path[] -> where the solution is being stored
-# which would later be copied to final_path[]
-def TSPRec(adj, curr_bound, curr_weight,
-           level, curr_path, visited):
-    global final_res
-
-    # base case is when we have reached level N
-    # which means we have covered all the nodes once
-    if level == N:
-
-        # check if there is an edge from
-        # last vertex in path back to the first vertex
-        if adj[curr_path[level - 1]][curr_path[0]] != 0:
-
-            # curr_res has the total weight
-            # of the solution we got
-            curr_res = curr_weight + adj[curr_path[level - 1]] \
-                [curr_path[0]]
-            if curr_res < final_res:
-                copyToFinal(curr_path)
-                final_res = curr_res
+    if profundidad == numero_nodos:
+        if adj[camino_actual[profundidad - 1]][camino_actual[0]] != 0:
+            curr_res = peso_actual + adj[camino_actual[profundidad - 1]] \
+                [camino_actual[0]]
+            if curr_res < costo:
+                crearFinal(camino_actual)
+                costo = curr_res
         return
 
-    # for any other level iterate for all vertices
-    # to build the search space tree recursively
-    for i in range(N):
+    for i in range(numero_nodos):
 
-        # Consider next vertex if it is not same
-        # (diagonal entry in adjacency matrix and
-        #  not visited already)
-        if (adj[curr_path[level - 1]][i] != 0 and
-                visited[i] == False):
-            temp = curr_bound
-            curr_weight += adj[curr_path[level - 1]][i]
+        if (adj[camino_actual[profundidad - 1]][i] != 0 and visitado[i] == False):
+            temp = bound_actual
+            peso_actual += adj[camino_actual[profundidad - 1]][i]
 
-            # different computation of curr_bound
-            # for level 2 from the other levels
-            if level == 1:
-                curr_bound -= ((firstMin(adj, curr_path[level - 1]) +
-                                firstMin(adj, i)) / 2)
+
+            if profundidad == 1:
+                bound_actual -= ((primerMinimo(adj, camino_actual[profundidad - 1]) + primerMinimo(adj, i)) / 2)
             else:
-                curr_bound -= ((secondMin(adj, curr_path[level - 1]) +
-                                firstMin(adj, i)) / 2)
+                bound_actual -= ((segundoMinimo(adj, camino_actual[profundidad - 1]) + primerMinimo(adj, i)) / 2)
 
-                # curr_bound + curr_weight is the actual lower bound
-            # for the node that we have arrived on.
-            # If current lower bound < final_res,
-            # we need to explore the node further
-            if curr_bound + curr_weight < final_res:
-                curr_path[level] = i
-                visited[i] = True
+            if bound_actual + peso_actual < costo:
+                camino_actual[profundidad] = i
+                visitado[i] = True
 
-                # call TSPRec for the next level
-                TSPRec(adj, curr_bound, curr_weight,
-                       level + 1, curr_path, visited)
+                rec(adj, bound_actual, peso_actual, profundidad + 1, camino_actual, visitado)
 
-                # Else we have to prune the node by resetting
-            # all changes to curr_weight and curr_bound
-            curr_weight -= adj[curr_path[level - 1]][i]
-            curr_bound = temp
+            peso_actual -= adj[camino_actual[profundidad - 1]][i]
+            bound_actual = temp
 
-            # Also reset the visited array
-            visited = [False] * len(visited)
-            for j in range(level):
-                if curr_path[j] != -1:
-                    visited[curr_path[j]] = True
+            visitado = [False] * len(visitado)
+            for j in range(profundidad):
+                if camino_actual[j] != -1:
+                    visitado[camino_actual[j]] = True
 
+def BranchBound(adj):
+    bound_actual = 0
+    camino_actual = [-1] * (numero_nodos + 1)
+    visitados = [False] * numero_nodos
 
-# This function sets up final_path
-def TSP(adj):
-    # Calculate initial lower bound for the root node
-    # using the formula 1/2 * (sum of first min +
-    # second min) for all edges. Also initialize the
-    # curr_path and visited array
-    curr_bound = 0
-    curr_path = [-1] * (N + 1)
-    visited = [False] * N
+    for i in range(numero_nodos):
+        bound_actual += (primerMinimo(adj, i) +
+                         segundoMinimo(adj, i))
 
-    # Compute initial bound
-    for i in range(N):
-        curr_bound += (firstMin(adj, i) +
-                       secondMin(adj, i))
+    bound_actual = math.ceil(bound_actual / 2)
 
-        # Rounding off the lower bound to an integer
-    curr_bound = math.ceil(curr_bound / 2)
+    visitados[0] = True
+    camino_actual[0] = 0
 
-    # We start at vertex 1 so the first vertex
-    # in curr_path[] is 0
-    visited[0] = True
-    curr_path[0] = 0
-
-    # Call to TSPRec for curr_weight
-    # equal to 0 and level 1
-    TSPRec(adj, curr_bound, 0, 1, curr_path, visited)
+    rec(adj, bound_actual, 0, 1, camino_actual, visitados)
 
 
-# Driver code
+adj_matrix = [[0, 10, 15, 20],
+              [10, 0, 35, 25],
+              [15, 35, 0, 30],
+              [20, 25, 30, 0]]
 
-# Adjacency matrix for the given graph
-adj = [[0, 10, 15, 20],
-       [10, 0, 35, 25],
-       [15, 35, 0, 30],
-       [20, 25, 30, 0]]
-N = 4
+#adj_matrix = [[0, 10, 15, 20],
+#              [10, 0, 35, 25],
+#              [15, 35, 0, 30],
+#              [20, 25, 30, 0]]
 
-# final_path[] stores the final solution
-# i.e. the // path of the salesman.
-final_path = [None] * (N + 1)
+#adj_matrix = [[0, 10, 15, 20],
+#              [10, 0, 35, 25],
+#              [15, 35, 0, 30],
+#              [20, 25, 30, 0]]
 
-# visited[] keeps track of the already
-# visited nodes in a particular path
-visited = [False] * N
+numero_nodos = 4
 
-# Stores the final minimum weight
-# of shortest tour.
-final_res = maxsize
+camino_final = [None] * (numero_nodos + 1)
 
-TSP(adj)
+visitados = [False] * numero_nodos
 
-print("Minimum cost :", final_res)
+costo = limite_superior
+
+BranchBound(adj_matrix)
+
+print("Minimum cost :", costo)
 print("Path Taken : ", end=' ')
-for i in range(N + 1):
-    print(final_path[i], end=' ')
+for i in range(numero_nodos + 1):
+    print(camino_final[i], end=' ')
